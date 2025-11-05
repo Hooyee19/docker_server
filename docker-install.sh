d1439bb4eae48047df058ca2391a84efe7875d4c
Hooyee19 <Abbeychuku51@gmail.com>
2025-11-04 20:50:37 -0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
-sudo apt-get update -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-key fingerprint 0EBFCD88
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
