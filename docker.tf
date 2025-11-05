353c2630ff99debd9bad4ed6d624096655fa68ec
Hooyee19 <Abbeychuku51@gmail.com>
2025-11-04 18:19:04 -0800

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # Create a remote backend for your terraform 
 terraform {
   backend "s3" {
-    bucket = "austinobioma-docker-tfstate"
+    bucket = "zaraoye-docker-tfstate"
     dynamodb_table = "app-state"
     key    = "LockID"
     region = "us-east-1"
